d2ff2e05b97537d0e0292d96f76476d4d89209af
André Henrique <andreh3nrique@gmail.com>
2019-12-02 22:20:11 -0300

    script sql

--- uml-requisitos/slq-previa/MeAjudaAqui.sql
+++ uml-requisitos/slq-previa/MeAjudaAqui.sql
@@ -1,1 +1,24 @@
 create database meajudaaqui;
+create table usuario(
+id_usuario int not null auto_increment,
+nome varchar(30),
+email varchar(30),
+senha varchar(32),
+tipo_usuario int,
+primary key(id_usuario)
+);
+create table problema(
+id_problema int not null auto_increment,
+tipo_problema int not null,
+data_solicitacao datetime,
+descricao_texto varchar(256),
+primary key(id_problema)
+);
+create table atendimento(
+id_atendimento int not null auto_increment,
+data_atendimento datetime,
+prazo_estabelecido date,
+status_conserto int,
+descricao_atendimento varchar(256),
+primary key(id_atendimento)
+);
