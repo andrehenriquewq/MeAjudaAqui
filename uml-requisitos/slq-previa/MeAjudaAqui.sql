974e7026339b04e8807e6ae60c7901b85711f9c5
André Henrique <andreh3nrique@gmail.com>
2019-12-03 22:41:51 -0300

    modelagem mysql finalizada

--- uml-requisitos/slq-previa/MeAjudaAqui.sql
+++ uml-requisitos/slq-previa/MeAjudaAqui.sql
@@ -1,24 +1,60 @@
-create database meajudaaqui;
-create table usuario(
-id_usuario int not null auto_increment,
-nome varchar(30),
-email varchar(30),
-senha varchar(32),
-tipo_usuario int,
-primary key(id_usuario)
-);
-create table problema(
-id_problema int not null auto_increment,
-tipo_problema int not null,
-data_solicitacao datetime,
-descricao_texto varchar(256),
-primary key(id_problema)
-);
-create table atendimento(
-id_atendimento int not null auto_increment,
-data_atendimento datetime,
-prazo_estabelecido date,
-status_conserto int,
-descricao_atendimento varchar(256),
-primary key(id_atendimento)
-);
+
+CREATE DATABASE meajudaaqui
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
+USE meajudaaqui;
+
+CREATE TABLE atendimento (
+  id_atendimento INT(11) NOT NULL AUTO_INCREMENT,
+  data_atendimento DATETIME NOT NULL,
+  prazo_estabelecido DATE NOT NULL,
+  status_conserto INT(11) NOT NULL,
+  descricao_atendimento VARCHAR(256) NULL,
+  PRIMARY KEY (id_atendimento))
+  DEFAULT CHARSET = utf8;
+
+CREATE TABLE usuario (
+  id_usuario INT(11) NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(30) NOT NULL,
+  email VARCHAR(30) NOT NULL,
+  senha VARCHAR(32) NOT NULL,
+  tipo_usuario INT(11) NOT NULL,
+  PRIMARY KEY (id_usuario)
+  )
+DEFAULT CHARSET = utf8;
+
+CREATE TABLE problema (
+  id_problema INT NOT NULL AUTO_INCREMENT,
+  tipo_problema INT NOT NULL,
+  data_solicitacao DATETIME NOT NULL,
+  descricao_texto VARCHAR(256),
+  fk_id_atendimento INT NOT NULL,
+  fk_id_usuario INT NOT NULL,
+  PRIMARY KEY (id_problema),
+  FOREIGN KEY (fk_id_atendimento)
+  REFERENCES atendimento (id_atendimento),
+  FOREIGN KEY (fk_id_usuario)
+  REFERENCES usuario (id_usuario)
+   )
+    DEFAULT CHARSET = utf8;
+
+CREATE TABLE tecnico (
+  id_tecnico INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(60) NOT NULL,
+  tipo_tecnico VARCHAR(20) NOT NULL,
+  ocupado TINYINT NOT NULL,
+  PRIMARY KEY (id_tecnico)
+  )
+ DEFAULT CHARSET = utf8;
+
+CREATE TABLE atendimento_tecnico (
+  atendimento_id_atendimento INT NOT NULL,
+  tecnico_id_tecnico INT NOT NULL,
+  PRIMARY KEY (atendimento_id_atendimento, tecnico_id_tecnico),
+  FOREIGN KEY (atendimento_id_atendimento)
+  REFERENCES atendimento (id_atendimento),
+  FOREIGN KEY (tecnico_id_tecnico)
+  REFERENCES tecnico (id_tecnico)
+  )
+DEFAULT CHARSET = utf8;
+
