729ab606e5288b124b34455e0d4db4ece9d43565
misakimeix <j3nnif3rsousa@gmail.com>
2019-12-16 11:23:34 -0300

    add modal views

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,12 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
- */
-/*
 Route::group(['prefix' => 'user'], function () {
     Route::get('/', 'User_control\\UserController@home');
     Route::get('/informar-problema', 'User_control\\UserController@informar_problema');
     Route::get('/solicitacoes-pendentes', 'User_control\\UserController@solicitacoes_pendentes');
 }); */
+Route::view('/h', 'view_user/table-historico');
 
 Route::resource('/user', 'User_Control\\User_m2a');
 
